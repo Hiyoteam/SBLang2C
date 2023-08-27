87eb1f602710d3ad7e14e4cd6ce9fe33684d7dac
MaggieLOL <tanhanze@qq.com>
2023-08-27 13:33:52 +0800

    little error fix

--- sblang_builtin_funcs/json.h
+++ sblang_builtin_funcs/json.h
@@ -5,5 +5,5 @@ string get_json_value(string json_text,string key){
     return json::parse(json_text)[key].get<std::string>();
 }
 string json_to_string(json object){
-    return object.dump()
+    return object.dump();
 }
