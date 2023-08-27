8bf200b65cedec812410c13be7988303abd7c4f1
MaggieLOL <tanhanze@qq.com>
2023-08-27 13:25:19 +0800

    Add json_to_string

--- sblang_builtin_funcs/json.h
+++ sblang_builtin_funcs/json.h
@@ -4,3 +4,6 @@ using namespace std;
 string get_json_value(string json_text,string key){
     return json::parse(json_text)[key].get<std::string>();
 }
+string json_to_string(json object){
+    return object.dump()
+}
